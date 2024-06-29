628ce2b615200a1d3674b26dcdc559bdd0cbc04f
Anton Müller <antxn.07@gmail.com>
2024-06-29 21:43:05 +0200

    Fixed UniformBuffer

--- Opxel/Graphics/UniformBuffer.cs
+++ Opxel/Graphics/UniformBuffer.cs
@@ -21,20 +21,24 @@ namespace Opxel.Graphics
             this.Buffer = new GraphicBuffer(BufferTarget.UniformBuffer, BufferUsageHint.StreamDraw);
             BlockIndex = GL.GetUniformBlockIndex(ShaderProgram.Handle, BlockName);
             GL.UniformBlockBinding(ShaderProgram.Handle, BlockIndex, bindingPoint);
-            GL.BindBufferBase(BufferRangeTarget.UniformBuffer, 0, Buffer.Handle);
+            GL.BindBufferBase(BufferRangeTarget.UniformBuffer, BindingPoint, Buffer.Handle);
             disposed = false;
         }
 
 
         public void BindBufferBase()
         {
-            GL.BindBufferBase(BufferRangeTarget.UniformBuffer, BlockIndex, Buffer.Handle);
+            GL.BindBufferBase(BufferRangeTarget.UniformBuffer, BindingPoint, Buffer.Handle);
         }
 
-        public unsafe void SetData<T>(T blockData) where T : unmanaged
+        public unsafe void SetData<T>(T[] blockData) where T : unmanaged
         {
-            Buffer.Bind();
-            GL.BufferData<T>(BufferTarget.UniformBuffer, sizeof(T), new T[] { blockData }, BufferUsageHint.DynamicDraw);
+            Buffer.SetData<T>(blockData);
+        }
+
+        public void SetData<T>(T blockData) where T : unmanaged
+        {
+            Buffer.SetData<T>(new T[] { blockData });
         }
 
         public void Dispose()
