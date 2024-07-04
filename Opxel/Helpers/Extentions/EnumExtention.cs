e0c0edbd9c239d881c58e47ec1c487563331940b
Anton Müller <antxn.07@gmail.com>
2024-07-04 23:57:27 +0200

    Added new extention method for easier logging

---
+++ Opxel/Helpers/Extentions/EnumExtention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opxel.Helpers.Extentions
+{
+    internal static class EnumExtention
+    {
+        public static string GetEnumName(this Enum @enum)
+        {
+            return Convert.ToString(@enum) ?? "undefined Enum";
+        }
+
+    }
+}
