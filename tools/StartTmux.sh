d134456a25063724f958bbb8c3fd5089a07a2482
Anton Müller <antxn.07@gmail.com>
2025-09-06 19:14:48 +0200

    fix: Make StartTmux.sh path independent
    Ensures the script correctly handles relative paths by changing to its own directory upon execution.

--- tools/StartTmux.sh
+++ tools/StartTmux.sh
@@ -4,6 +4,9 @@ session="Opxel"
 nvim_title=" NVIM"
 build_title=" BUILD"
 git_title="󰊢 GIT"
+gemini_title="󰫣 GEMINI"
+
+cd "$(dirname "$0")"
 
 if ! tmux has-session -t "$session" 2>/dev/null; then 
     tmux new-session -d -s "$session" -n "$nvim_title"
@@ -14,6 +17,9 @@ if ! tmux has-session -t "$session" 2>/dev/null; then
 
     tmux new-window -t "$session:2" -n "$git_title"
     tmux send-keys -t "$session:$git_title" 'cd .. && clear && git status' C-m
+
+    tmux new-window -t "$session:3" -n "$gemini_title"
+    tmux send-keys -t "$session:$gemini_title" 'cd .. && clear && gemini' C-m
 fi
 
 tmux attach-session -t "$session"
